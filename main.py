24b61752f369da51d462f3f011e511166755cd38
LuisCarlosOrobio <140061957+LuisCarlosOrobio@users.noreply.github.com>
2023-11-17 13:01:41 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, HTTPException
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 import websockets
 import asyncio
 import httpx
+from starlette.websockets import WebSocketDisconnect
+import json
 
 app = FastAPI()
 
@@ -19,7 +21,19 @@ async def get_root():
 async def transcribe_audio(audio_data):
     async with websockets.connect("ws://localhost:5000/ws") as ws:
         await ws.send(audio_data)
-        return await ws.recv()
+        task_id = await ws.recv()
+
+        # Fetch the result of the transcription task
+        async with httpx.AsyncClient() as client:
+            response = await client.get(f"http://localhost:5000/result/{task_id}")
+            if response.status_code == 200:
+                result = response.json()
+                if result['status'] == 'SUCCESS':
+                    return result['result']
+                else:
+                    return "Transcription in progress or failed"
+            else:
+                raise HTTPException(status_code=response.status_code, detail="Failed to fetch task result.")
 
 # WebSocket endpoint for the frontend
 @app.websocket("/ws")
@@ -29,12 +43,12 @@ async def websocket_endpoint(websocket: WebSocket):
         try:
             # Receive audio data from the frontend
             audio_data = await websocket.receive_bytes()
-            
+
             # Transcribe the audio using the external service
             transcribed_text = await transcribe_audio(audio_data)
-            
+
             # Send the transcribed text back to the frontend
-            await websocket.send_text(transcribed_text)
+            await websocket.send_text(json.dumps({"text": transcribed_text}))
 
         except websockets.exceptions.ConnectionClosed as e:
             print(f"WebSocket connection closed: {e}")
@@ -42,11 +56,11 @@ async def websocket_endpoint(websocket: WebSocket):
         except Exception as e:
             print(f"An error occurred: {e}")
             break
-            
+
 @app.get("/status/{task_id}")
 async def get_task_status(task_id: str):
     async with httpx.AsyncClient() as client:
-        response = await client.get(f"http://localhost:5001/status/{task_id}")
+        response = await client.get(f"http://localhost:5000/status/{task_id}")
         if response.status_code == 200:
             return response.json()
         else:
@@ -55,7 +69,7 @@ async def get_task_status(task_id: str):
 @app.get("/result/{task_id}")
 async def get_task_result(task_id: str):
     async with httpx.AsyncClient() as client:
-        response = await client.get(f"http://localhost:5001/result/{task_id}")
+        response = await client.get(f"http://localhost:5000/result/{task_id}")
         if response.status_code == 200:
             return response.json()
         else:
