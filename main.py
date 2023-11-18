75afce09922e4cd83762e8e181a997731232da6e
LuisCarlosOrobio <140061957+LuisCarlosOrobio@users.noreply.github.com>
2023-11-18 07:05:12 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.staticfiles import StaticFiles
 import httpx
 import json
 import random
+import websockets
 
 app = FastAPI()
 
@@ -15,51 +16,64 @@ async def get_root():
     with open("static/index.html", "r") as f:
         return HTMLResponse(content=f.read())
 
-# Define URLs for external services
-image_processing_service_url = "http://127.0.0.1:5001/completion"
 text_processing_service_url = "http://127.0.0.1:5002/completion"
+image_processing_service_url = "http://127.0.0.1:5001/completion"
+
+async def transcribe_audio(audio_data):
+    async with websockets.connect("ws://localhost:5000/ws") as ws:
+        await ws.send(audio_data)
+        task_id = await ws.recv()
+
+        async with httpx.AsyncClient() as client:
+            response = await client.get(f"http://localhost:5000/result/{task_id}")
+            if response.status_code == 200:
+                result = response.json()
+                if result['status'] == 'SUCCESS':
+                    return result['result']
+                else:
+                    return "Transcription in progress or failed"
+            else:
+                raise HTTPException(status_code=response.status_code, detail="Failed to fetch task result.")
+
+async def send_text_for_processing(text):
+    # Prepare the data payload as JSON with all specified parameters
+    data = json.dumps({
+        "prompt": text,
+        "n_predict": 20,
+        "sampling": {
+            "repeat_last_n": 64,
+            "repeat_penalty": 1.1,
+            "presence_penalty": 0.0,
+            "frequency_penalty": 0.0,
+            "top_k": 40,
+            "tfs_z": 1.0,
+            "top_p": 0.95,
+            "typical_p": 1.0,
+            "temperature": 0.7,
+            "mirostat": 0,
+            "mirostat_lr": 0.1,
+            "mirostat_ent": 5.0,
+            "stream": True
+        }
+    })
+
+def generate_random_id():
+    return random.randint(1, 1000000)
 
 @app.post("/process-data")
 async def process_data(request: Request):
     data = await request.json()
 
-    if "image_data" in data and "text" in data:
-        # Both image and transcribed text available
-        url = image_processing_service_url
-        payload = {
-            "prompt": data["text"],
-            "image_data": [{"data": data["image_data"], "id": generate_random_id()}]
-        }
-    elif "image_data" in data:
-        # Only image available
+    if "image_data" in data:
         url = image_processing_service_url
         payload = {
             "image_data": [{"data": data["image_data"], "id": generate_random_id()}]
         }
-    elif "text" in data:
-        # Only transcribed text available
-        url = text_processing_service_url
-        payload = {
-            "prompt": data["text"],
-            "n_predict": 20,
-            "sampling": {
-                "repeat_last_n": 64,
-                "repeat_penalty": 1.1,
-                "presence_penalty": 0.0,
-                "frequency_penalty": 0.0,
-                "top_k": 40,
-                "tfs_z": 1.0,
-                "top_p": 0.95,
-                "typical_p": 1.0,
-                "temperature": 0.7,
-                "mirostat": 0,
-                "mirostat_lr": 0.1,
-                "mirostat_ent": 5.0,
-                "stream": True
-            }
-        }
+        if "text" in data:
+            payload["prompt"] = data["text"]
+
     else:
-        return JSONResponse(content={"error": "No valid data received"}, status_code=400)
+        return JSONResponse(content={"error": "No valid image data received"}, status_code=400)
 
     async with httpx.AsyncClient() as client:
         response = await client.post(url, json=payload)
@@ -68,32 +82,39 @@ async def process_data(request: Request):
         else:
             return JSONResponse(content={"error": "Failed to process data"}, status_code=response.status_code)
 
-def generate_random_id():
-    return random.randint(1, 1000000)                                                                                                                                                                                                    
 
-@app.websocket("/ws")                                                                                                                                                                                                                                                                   
-async def websocket_endpoint(websocket: WebSocket):                                                                                                                                                                                                                                     
-    await websocket.accept()                                                                                                                                                                                                                                                            
-    while True:                                                                                                                                                                                                                                                                         
-        try:                                                                                                                                                                                                                                                                            
+    async with httpx.AsyncClient() as client:
+        response = await client.post(text_processing_service_url, headers={'Content-Type': 'application/json'}, content=data)
+        if response.status_code == 200:
+            decoded_response = response.json()
+            return decoded_response.get('content')
+        else:
+            return f"Error: Received response code {response.status_code}"
+
+        # WebSocket endpoint for the frontend                                                                                                                                                                                                                                                   
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        try:
             # Receive audio data from the frontend                                                                                                                                                                                                                                      
-            audio_data = await websocket.receive_bytes()                                                                                                                                                                                                                                
-                                                                                                                                                                                                                                                                                        
+            audio_data = await websocket.receive_bytes()
+
             # Transcribe the audio using the external Whisper service                                                                                                                                                                                                                   
-            async with websockets.connect("ws://localhost:5000/ws") as ws:                                                                                                                                                                                                              
-                await ws.send(audio_data)                                                                                                                                                                                                                                               
-                transcribed_text = await ws.recv()                                                                                                                                                                                                                                      
-                                                                                                                                                                                                                                                                                        
+            async with websockets.connect("ws://localhost:5000/ws") as ws:
+                await ws.send(audio_data)
+                transcribed_text = await ws.recv()
+
             # Send the transcribed text back to the frontend                                                                                                                                                                                                                            
-            await websocket.send_text(json.dumps({"text": transcribed_text}))                                                                                                                                                                                                           
-                                                                                                                                                                                                                                                                                        
-            processed_text = await send_text_for_processing(transcribed_text)                                                                                                                                                                                                           
-                                                                                                                                                                                                                                                                                        
-            await websocket.send_text(json.dumps({"processed_text": processed_text}))                                                                                                                                                                                                   
-                                                                                                                                                                                                                                                                                        
-        except websockets.exceptions.ConnectionClosed as e:                                                                                                                                                                                                                             
-            print(f"WebSocket connection closed: {e}")                                                                                                                                                                                                                                  
-            break                                                                                                                                                                                                                                                                       
-        except Exception as e:                                                                                                                                                                                                                                                          
-            print(f"An error occurred: {e}")                                                                                                                                                                                                                                            
-            break     
+            await websocket.send_text(json.dumps({"text": transcribed_text}))
+
+            processed_text = await send_text_for_processing(transcribed_text)
+
+            await websocket.send_text(json.dumps({"processed_text": processed_text}))
+
+        except websockets.exceptions.ConnectionClosed as e:
+            print(f"WebSocket connection closed: {e}")
+            break
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            break
